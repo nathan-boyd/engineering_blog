6b68348df6bd26847d6712fa63422abecd21e40d
Nathan Boyd <nathan@engineernathan.org>
2023-01-22 17:15:44 -0500

    Update post script and config for analytics tracking
    - Updated post script to replace spaces in post name with hyphens
    - Changed `googleAnalytics` code in `config.yaml` from `UA-90040103-1` to `G-J5HMDX8CT9`
    
    [scripts/post.sh]
    - Replace spaces in post name with hyphens
    - Add post title variable
    - Create an img folder with post title name
    - Create post with post title name
    [config.yaml]
    - Change `googleAnalytics` code from `UA-90040103-1` to `G-J5HMDX8CT9`

--- scripts/post.sh
+++ scripts/post.sh
@@ -3,5 +3,7 @@
 POST_NAME=$1
 POST_NAME="${POST_NAME// /-}"
 TODAY="$(date +'%Y-%m-%d')"
+POST_TITLE="$TODAY-$POST_NAME"
 
-hugo new posts/$TODAY-$POST_NAME.md
+mkdir "img/$POST_TITLE"
+hugo new "posts/$POST_TITLE.md"
