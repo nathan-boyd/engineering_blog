33e85e204fa04e434e99e9ec4dd467b39d7a00e9
Nathan Boyd <nathan@engineernathan.org>
2023-01-22 22:44:32 -0500

    Improve deployment script and config
    - Add error handling to deploy script
    - Add Hugo version check
    - Add build and deploy commands
    - Add logging info
    
    [scripts/deploy.sh]
    - Add set -e for error handling
    - Hugo version check added
    - Build command added
    - Deploy command added
    - Logging info added

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-hugo && hugo deploy --target=s3
+set -e
+
+logInfo "hugo version"
+hugo --version
+
+logInfo "building"
+hugo
+
+logInfo "deploying"
+hugo deploy --target=s3
+
+logInfo "done"
